b0fc65b486a3e8ea9dd3f7aec51cc0f3c631f976
beshari jamal <beshjm@gmail.com>
2023-10-05 14:23:41 -0400

    assignment 4-1, second attempt

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -50,21 +50,25 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
+    bool success = false;
     struct thread_data* thread_data = malloc(sizeof(struct thread_data));
     thread_data_init(thread_data, mutex, wait_to_obtain_ms, wait_to_release_ms);
     int rc = pthread_create(thread, NULL, threadfunc, (void*) thread_data);
-    if (rc == 0) {
+    if (rc != 0) {
+        ERROR_LOG("pthread_create failed with error %d", rc);
+        success = false;
+    }
+    else {
         int rc = pthread_join(*thread, NULL);
         if (rc != 0) {
             ERROR_LOG("pthread_join failed with error %d", rc);
-            return false;
+            success = false;
         }
-        free (thread_data);
-        return true;
+        else {
+            success = true;
+        }
     }
-
-    ERROR_LOG("pthread_create failed with error %d", rc);
     free (thread_data);
-    return false;
+    return success;
 }
 
